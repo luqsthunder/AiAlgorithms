ad4b7070f9222c6dc8d1ed819801439d3526b4bf
luqsthunder <lucasthund3r@gmail.com>
2018-03-21 09:36:43 -0300

    knn and normalization template design

--- knn/src/knn.cpp
+++
@@ -1,2 +0,0 @@
-#include "knn.h"
-
--- knn/src/knn.h
+++ knn/src/knn.h
@@ -1,15 +1,34 @@
 #pragma once
 
-#include <Eigen/Eigen>
+#include <vector>
+#include <eigen3/Eigen/Eigen>
 
 namespace aia
 {
+namespace normalization
+{
 
-template<typename T,  typename container = std::vector<T>>
+template<typename DB>
+struct identity
+{
+  constexpr void operator()(DB&& database) {  }
+
+};
+
+}
+
+template<typename T,template <typename,
+                              typename = std::allocator<T>>
+                              class container = std::vector,
+                              typename norm = normalization::identity>
 class Knn
 {
 public:
-  Knn();
+  Knn(const container<T>& database, const container<T>& cls);
+
+  void train();
 };
+
+#include "knn.cpp"
 
 }
